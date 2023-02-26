a61c54b7cca7585bd3ccd2befa75924cd3e6d7b0
udhayarajan <mudhayarajan.2013@gmail.com>
2023-02-26 22:47:48 +0530

    Final CICD with auto close and release sonatype

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,5 @@
 import android.annotation.SuppressLint
+import java.util.Properties
 
 /*
  *    Copyright (c) 2023 Udhayarajan M
@@ -21,12 +22,14 @@ plugins {
     kotlin("multiplatform") version "1.7.22"
     id("convention.publication")
     id("com.android.library")
+    id("io.github.gradle-nexus.publish-plugin") version "1.2.0"
 }
 
 group = "io.github.udhayarajan"
 version = "3.3.2"
 //Version Naming incremented if "<NEW_FEATURE_ADDED>.<WORKED_ON_BUG>.<BETA_VERSION_COUNT>"
 //Priority on incrementing Feature > BugFix > Beta
+
 repositories {
     google()
     mavenCentral()
@@ -106,4 +109,44 @@ dependencies {
     testImplementation(project(mapOf("path" to ":")))
     androidTestImplementation(project(mapOf("path" to ":")))
     androidTestImplementation(project(mapOf("path" to ":")))
+}
+nexusPublishing{
+// Stub secrets to let the project sync and build without the publication values set up
+    val ext = rootProject.ext
+    ext["signing.keyId"] = null
+    ext["signing.password"] = null
+    ext["sonatypeStagingProfileId"] = null
+    ext["signing.key"] = null
+    ext["ossrhUsername"] = null
+    ext["ossrhPassword"] = null
+    ext["sonatypeStagingProfileId"] = null
+
+// Grabbing secrets from local.properties file or from environment variables, which could be used on CI
+    val secretPropsFile = project.rootProject.file("local.properties")
+    if (secretPropsFile.exists()) {
+        secretPropsFile.reader().use {
+            Properties().apply {
+                load(it)
+            }
+        }.onEach { (name, value) ->
+            ext[name.toString()] = value
+            println(ext)
+        }
+    } else {
+        ext["signing.keyId"] = System.getenv("SIGNING_KEY_ID")
+        ext["signing.password"] = System.getenv("SIGNING_PASSWORD")
+        ext["signing.key"] = System.getenv("SIGNING_KEY")
+        ext["ossrhUsername"] = System.getenv("OSSRH_USERNAME")
+        ext["ossrhPassword"] = System.getenv("OSSRH_PASSWORD")
+        ext["sonatypeStagingProfileId"] = System.getenv("SONATYPE_STAGING_PROFILE_ID")
+    }
+    repositories {
+        sonatype {
+            stagingProfileId.set(ext["sonatypeStagingProfileId"].toString())
+            username.set(ext["ossrhUsername"].toString())
+            password.set(ext["ossrhPassword"].toString())
+            nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
+            snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
+        }
+    }
 }
