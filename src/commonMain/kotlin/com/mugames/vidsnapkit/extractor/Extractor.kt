206768766c001a5a1f39d27a51c59c4b2c5062db
udhayarajan <mudhayarajan.2013@gmail.com>
2023-07-19 11:30:43 +0530

    test: testing the flow & pipeline

--- src/commonMain/kotlin/com/mugames/vidsnapkit/extractor/Extractor.kt
+++ src/commonMain/kotlin/com/mugames/vidsnapkit/extractor/Extractor.kt
@@ -257,11 +257,11 @@ abstract class Extractor(
                 "Safari/537.36",
             "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.157 " +
                 "Safari/537.36",
-            "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.157 " +
-                "Safari/537.36",
             "Mozilla/5.0 (iPhone; CPU iPhone OS 12_3_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) " +
-                "Mobile/15E148 Instagram 105.0.0.11.118 (iPhone11,8; iOS 12_3_1; en_US; en-US; scale=2.00; " +
-                "828x1792; 165586599)"
+                    "Mobile/15E148 Instagram 105.0.0.11.118 (iPhone11,8; iOS 12_3_1; en_US; en-US; scale=2.00; " +
+                    "828x1792; 165586599)",
+            "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.157 " +
+                "Safari/537.36"
         )
         return userAgents.random()
     }
